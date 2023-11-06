e179ef12e2ac1af3b9727b80c24777d5a44d0490
burak demirer <burakdemirer010@gmail.com>
2023-11-06 23:30:12 +0300

    mysqldatabase

--- envanter/settings.py
+++ envanter/settings.py
@@ -78,11 +78,16 @@ WSGI_APPLICATION = 'envanter.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'mydb',
+		'USER': 'root',
+		'PASSWORD': 'admin',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
+
 
 
 # Password validation
